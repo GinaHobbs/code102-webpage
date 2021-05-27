d8f8b0679c7c4f2a85f9f71f62a96a89f8ab2e73
GinaHobbs <ginamariehobbs@gmail.com>
2021-05-26 21:53:43 -0500

    added change system color option

--- app.js
+++ app.js
@@ -5,15 +5,33 @@
       }
 
       function randomFF4Image() {
-        random = randomNumber();
-        console.log(random);
-        if (random == 1){
+        imageChoice = randomNumber();
+        console.log(imageChoice);
+        if (imageChoice == 1){
           document.getElementById("FF4_logo").innerHTML = '<img src="FF4_red_wings.jpeg"> <br> <button type="button" onclick="randomFF4Image()">Random Image!</button>' ;
-        } else if (random == 2) {
+        } else if (imageChoice == 2) {
           document.getElementById("FF4_logo").innerHTML = '<img src="ff4_cartridge.jpeg"> <br> <button type="button" onclick="randomFF4Image()">Random Image!</button>' ;
-        } else if (random == 3) {
+        } else if (imageChoice == 3) {
           document.getElementById("FF4_logo").innerHTML = '<img src="FF4_snes_menu.png"> <br> <button type="button" onclick="randomFF4Image()">Random Image!</button>' ;
         } else {
           document.getElementById("FF4_logo").innerHTML = '<img src="FF4_logo.png"> <br>  <button type="button" onclick="randomFF4Image()">Random Image!</button>' ;
+        }
+      }
+
+      function changeSystemColor() {
+        // document.getElementById("system_h1").style.backgroundColor = "black";
+        var colorChoice = randomNumber();
+        if (colorChoice == 1) {
+          color = "purple";
+        } else if (colorChoice == 2) {
+          color = "darkgreen";
+        } else if (colorChoice == 3) {
+          color = "darkred";
+        } else {
+          color = "black";
+        }
+        var elements = document.getElementsByClassName("system");
+        for (var i in elements) {
+           elements[i].style.backgroundColor = color;
         }
       }
