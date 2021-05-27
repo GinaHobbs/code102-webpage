ffe43c0d3f87cd0b852a86da03c19ea5f8197bb9
GinaHobbs <ginamariehobbs@gmail.com>
2021-05-27 15:03:52 -0500

    complted functions

--- app.js
+++ app.js
@@ -7,18 +7,20 @@
       function randomFF4Image() {
         imageChoice = randomNumber();
         console.log(imageChoice);
+        button = '<button type="button" onclick="randomFF4Image()">Random Image!</button>'
         if (imageChoice == 1){
-          document.getElementById("FF4_logo").innerHTML = '<img src="FF4_red_wings.jpeg"> <br> <button type="button" onclick="randomFF4Image()">Random Image!</button>' ;
+          document.getElementById("FF4_logo").innerHTML = '<img src="FF4_red_wings.jpeg"> <br>' + button ;
         } else if (imageChoice == 2) {
-          document.getElementById("FF4_logo").innerHTML = '<img src="ff4_cartridge.jpeg"> <br> <button type="button" onclick="randomFF4Image()">Random Image!</button>' ;
+          document.getElementById("FF4_logo").innerHTML = '<img src="ff4_cartridge.jpeg"> <br>' + button  ;
         } else if (imageChoice == 3) {
-          document.getElementById("FF4_logo").innerHTML = '<img src="FF4_snes_menu.png"> <br> <button type="button" onclick="randomFF4Image()">Random Image!</button>' ;
+          document.getElementById("FF4_logo").innerHTML = '<img src="FF4_snes_menu.png"> <br>' + button  ;
         } else {
-          document.getElementById("FF4_logo").innerHTML = '<img src="FF4_logo.png"> <br>  <button type="button" onclick="randomFF4Image()">Random Image!</button>' ;
+          document.getElementById("FF4_logo").innerHTML = '<img src="FF4_logo.png"> <br>' + button  ;
         }
       }
 
-      function changeSystemColor() {
+      // colorChoice = 0;
+      function changeSystemColor(colorChoice) {
         // document.getElementById("system_h1").style.backgroundColor = "black";
         var colorChoice = randomNumber();
         if (colorChoice == 1) {
@@ -33,5 +35,23 @@
         var elements = document.getElementsByClassName("system");
         for (var i in elements) {
            elements[i].style.backgroundColor = color;
+        }
+      }
+
+      function quiz() {
+        let bossAnswer = prompt('Who is the final boss of FF4?')
+        document.write('1) Who is the final boss of FF4?<br>Answer: Zeromus <br> You said: ' + bossAnswer)
+        if (bossAnswer.toLowerCase() == 'zeromus') {
+          document.write('<br>You are correct!');
+        } else {
+          document.write('<br>You are incorrect!');
+        }
+
+        let captainAnswer = prompt('What is Cecil the Captain of?')
+        document.write('<br><br>2) What is Cecil the Captain of?<br>Answer: The Red Wings <br> You said: ' + captainAnswer)
+        if (captainAnswer.toLowerCase() == 'the red wings') {
+          document.write('<br>You are correct!');
+        } else {
+          document.write('<br>You are incorrect!');
         }
       }
